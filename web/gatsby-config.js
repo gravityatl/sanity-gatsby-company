f7e406b2891d1e7ec8249913e75abe3351c7ce13
gravityatl <mweil@mindspring.com>
2020-01-06 14:18:26 -0500

    form-builder allow image uploads
    Form Builder Update

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -28,6 +28,31 @@ module.exports = {
         ],
         display: 'swap'
       }
+    },
+    {
+      resolve: "gatsby-plugin-seo",
+      options: {
+        siteName: "Example Company",
+        defaultSiteImage: "/img/logo.png",
+        siteUrl: "https://example.com",
+        twitterCreator: "@twitterhandle",
+        twitterSite: "@twitterhandle",
+        globalSchema: `{
+            "@type": "WebSite",
+            "@id": "https://example.com/#website",
+            "url": "https://example.com/",
+            "name": "Example Site Title",
+            "publisher": {
+              "@id": "https://example.com/about/#organization"
+            },
+            "image": {
+              "@type": "ImageObject",
+              "@id": "https://example.com/#logo",
+              "url": "https://example.com/img/logo.png",
+              "caption": "Example Company Logo"
+            }
+          }`
+      }
     }
   ]
 }
